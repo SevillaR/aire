312ffaad25934ffdea2292f1940d0460bca0b368
fbalao <fbalao@us.es>
2016-01-30 16:41:12 +0100

    Update función html2list

--- R/html2list.R
+++ R/html2list.R
@@ -1,3 +1,23 @@
-library(rvest)
+#' html2list
+#'
+#' @param query
+#'
+#' @return
+#' @export
+#' @import rvest
+#' @examples
+html2list<-function(query){
+require(rvest)
+#query<-"http://www.juntadeandalucia.es/medioambiente/atmosfera/informes_siva/ene15/nse150129.htm"
 air.dat <- read_html(query)
-html2list<-(html_table(html_nodes(air.dat, "table")[25], fill = T, header = T)[[1]])
+n_estaciones<-(length(html_nodes(air.dat, "table"))-1)/2
+air.list<-rep(list(NULL),n_estaciones)
+for (i in 1:n_estaciones){
+  air.list[[i]]$id  <-html_table(html_nodes(air.dat, "table")[i*2], fill = T, header = T)[[1]]
+  df <-html_table(html_nodes(air.dat, "table")[i*2+1], fill = T, header = T)[[1]]
+  df <- df[,!sapply(df, function (e) {all(is.na(e))} )]
+  df<-df[!apply(df[,-1],1,function (e) {all(is.na(e))}),]
+  air.list[[i]]$dat <-df
+    }
+return(air.list)
+}
