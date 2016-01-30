d1e6d70196c7a280ad781084fba1f1e6f069ed1e
Pakillo <f.rodriguez.sanc@gmail.com>
2016-01-30 17:46:26 +0100

    added help file for pkg

---
+++ R/aire-package.R
@@ -0,0 +1,10 @@
+#' Analysing air quality in Andalusia
+#'
+#' This package retrieves air quality data in Andalusia.
+#'
+#' @name aire-package
+#' @docType package
+#' @aliases aire
+#' @keywords package
+
+NULL
