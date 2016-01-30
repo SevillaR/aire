8847ca4c0030c3d3bef354bb260da79ba08f4a51
fbalao <fbalao@us.es>
2016-01-30 17:40:39 +0100

    Update de la función html2list

--- R/html2list.R
+++ R/html2list.R
@@ -1,18 +1,26 @@
-#' html2list
+#' @title html2list  Get
 #'
 #' @param query
 #'
 #' @return A list of lists (each element being a station).
 #' @export
+#' @author Francisco Viciana & Francisco Balao
 #' @import rvest
+#' @example
+#' query<-"http://www.juntadeandalucia.es/medioambiente/atmosfera/informes_siva/ene15/nse150129.htm"
+#' dat29.1.15<-html2list(query)
+#' @seealso list2df
+#' @details
 
 html2list<-function(query){
-#query<-"http://www.juntadeandalucia.es/medioambiente/atmosfera/informes_siva/ene15/nse150129.htm"
 air.dat <- read_html(query)
 n_estaciones<-(length(html_nodes(air.dat, "table"))-1)/2
 air.list<-rep(list(NULL),n_estaciones)
 for (i in 1:n_estaciones){
-  air.list[[i]]$id  <-html_table(html_nodes(air.dat, "table")[i*2], fill = T, header = T)[[1]]
+  id<-html_table(html_nodes(air.dat, "table")[i*2], fill = T, header = F)[[1]]
+  id <- structure(id[,2],names =id[,1])
+  #id<-sub(' ','' ,id) Hay que hacer funcion trim para eliminar espacios
+  air.list[[i]]$id<-id
   df <-html_table(html_nodes(air.dat, "table")[i*2+1], fill = T, header = T)[[1]]
   df <- df[,!sapply(df, function (e) {all(is.na(e))} )]
   df<-df[!apply(df[,-1],1,function (e) {all(is.na(e))}),]
