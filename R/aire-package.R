0fe4e0a757908bf785c563ed62aaa8ad71ffd28a
Pakillo <f.rodriguez.sanc@gmail.com>
2016-02-01 17:55:36 +0100

    updated pkg doc

--- R/aire-package.R
+++ R/aire-package.R
@@ -1,6 +1,8 @@
-#' Analysing air quality in Andalusia
+#' Grab data on air pollution in Andalusia
 #'
-#' This package retrieves air quality data in Andalusia.
+#' This package retrieves daily data on air quality in Andalusia (see http://bit.ly/airedata).
+#' Currently, data are available from 2005 onwards.
+#' The data are formatted so that they can be directly analysed with the 'openair' package.
 #'
 #' @name aire-package
 #' @docType package
