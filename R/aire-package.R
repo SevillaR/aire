b4703475356e417e59f404377c9a71d7dcf44da1
Pakillo <f.rodriguez.sanc@gmail.com>
2016-02-02 14:37:31 +0100

    include warning these data are not validated

--- R/aire-package.R
+++ R/aire-package.R
@@ -1,8 +1,11 @@
 #' Grab data on air pollution in Andalusia
 #'
-#' This package retrieves daily data on air quality in Andalusia (see http://bit.ly/airedata).
+#' This package can download and retrieve daily data on air quality in Andalusia (Spain),
+#' from any of the stations maintained by 'Consejeria de Medio Ambiente' (http://bit.ly/airedata).
+#' The data are formatted so that they can be directly analysed with the 'openair' package. Note these data
+#' are collected automatically, and not validated. Check our Consejeria de Medio Ambiente website for monthly
+#' and annual reports based on validated data.
 #' Currently, data are available from 2005 onwards.
-#' The data are formatted so that they can be directly analysed with the 'openair' package.
 #'
 #' @name aire-package
 #' @docType package
