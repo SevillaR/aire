817960e2e151d66697fe8dde59c4967b2a2f86d8
Pakillo <f.rodriguez.sanc@gmail.com>
2016-02-24 22:44:44 +0100

    added documentation for datasets

---
+++ R/se2015.R
@@ -0,0 +1,23 @@
+#' Air quality data for Sevilla province, year 2015.
+#'
+#' Air quality data for all stations in Sevilla province for year 2015.
+#'
+#' @docType data
+#' @name se2015
+#' @format A dataframe with 606911 rows and 11 variables:
+#' \describe{
+#'   \item{Provincia}{Province}
+#'   \item{Municipio}{Council where station is located}
+#'   \item{site}{Station name}
+#'   \item{Direccion}{Address}
+#'   \item{date}{Date and time of the measurement, in format "yyyy-mm-dd hh:mm:ss"}
+#'   \item{SO2}{SO2 concentration (in micrograms per cubic meter)}
+#'   \item{PART}{Particles concentration (in micrograms per cubic meter)}
+#'   \item{NO2}{NO2 concentration (in micrograms per cubic meter)}
+#'   \item{CO}{CO concentration (in micrograms per cubic meter)}
+#'   \item{O3}{O3 concentration (in micrograms per cubic meter)}
+#'   \item{SH2}{SH2 concentration (in micrograms per cubic meter)}
+#' }
+#' @source Consejeria de Medio Ambiente, Junta de Andalucia (\url{http://bit.ly/airedata})
+
+NULL
---
+++ R/stations.R
@@ -0,0 +1,17 @@
+#' Measurement stations info
+#'
+#' Coordinates and information of all measurement stations in Andalusia.
+#'
+#' @docType data
+#' @name stations
+#' @format A SpatialPointsDataFrame with 99 rows and 5 variables:
+#' \describe{
+#'   \item{X}{Station number}
+#'   \item{ESTACION}{Station name}
+#'   \item{MUNICIPIO}{Council}
+#'   \item{PROVINCIA}{Province}
+#'   \item{site}{Station name (used for compatibility with package openair)}
+#' }
+#' @source Consejeria de Medio Ambiente, Junta de Andalucia (\url{http://www.juntadeandalucia.es/medioambiente/portal_web/web/temas_ambientales/atmosfera/calidad_aire/RVCCAA_2015.pdf})
+
+NULL
