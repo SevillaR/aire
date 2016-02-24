d8d04818465fed61cc03002039b928e6f6d6d93b
Pakillo <f.rodriguez.sanc@gmail.com>
2016-02-24 15:13:49 +0100

    added vignette nubepolvo

---
+++ vignettes/nubepolvo.R
@@ -0,0 +1,32 @@
+## ----echo=FALSE----------------------------------------------------------
+library(knitr)
+opts_knit$set(root.dir=normalizePath('../'))
+opts_chunk$set(warning = FALSE, message = FALSE)
+opts_chunk$set(fig.path = "nubepolvo_figuras/", fig.show="hide")
+
+## ------------------------------------------------------------------------
+library(aire)
+
+## ------------------------------------------------------------------------
+datos <- get_daily_data(province = "se", from = "2016-02-15", to = "2016-02-23")
+
+## ------------------------------------------------------------------------
+library(openair)
+
+## ----summaryPlot---------------------------------------------------------
+summaryPlot(datos, pollutant = "PART", period = "months")
+
+## ----timePlot------------------------------------------------------------
+timePlot(datos, pollutant = "PART", type = "site")
+
+## ----calendarPlot--------------------------------------------------------
+calendarPlot(datos, pollutant = "PART")
+
+## ------------------------------------------------------------------------
+datos.21 <- get_daily_data("2016-02-21", "se")
+data(stations)
+datos.21.sp <- merge(datos.21, stations[, "site"], by = "site", all.x = TRUE)
+
+## ----map-----------------------------------------------------------------
+GoogleMapsPlot(datos.21.sp, pollutant = "PART")  
+
